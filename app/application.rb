890adcdc7e015efd9f4cc4e19db6f3b56136a4c5
Mark Kanko <mkanko66@gmail.com>
2019-06-07 18:40:31 -0700

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,20 +1,35 @@
 class Application
 
-    @@items = []
+    @@item = []
 
     def call(env)
         resp = Rack::Response.new
         req = Rack::Request.new(env)
 
-        if req.path.match(/items/)
-            if req.path == "/items"
-                resp.write "You requested items"
-            else
-                resp.write "Route not found"
-                resp.status = 404
-            end
+        if req.path.match("/search")
+            search_term = req.params["q"]
+            resp.write handle_search(search_term)
+            resp.status = 200
+        else
+            resp.write "Item Not found"
+            resp.status = 400
         end 
 
+        if req.path == "/items"
+            resp.write "You requested items"
+        else
+            resp.write "Route not found"
+            resp.status = 404
+        end
+
         resp.finish
-    end 
+    end
+    
+    def handle_search(search_term)
+        if @@item.include?(search_term)
+            return "#{search_term.price}"
+          else
+            return "Couldn't find #{search_term}"
+          end
+        end
 end 
